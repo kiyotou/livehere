3e1bba63c7a90ba338bba79776a4fc582fd415bd
kiyotou <kiyotou@gmail.com>
2011-11-17 15:35:35 +0900

    レイアウト微修正

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,6 @@
 function initialize() {
+
+	// set Google Map 
 	var latlng = new google.maps.LatLng(34.6718, 135.497841);
 	var myOptions = {
 	  zoom: 15,
@@ -6,6 +8,27 @@ function initialize() {
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById("map_minami"), myOptions);
+
+	// read tweets
+	$.getJSON(
+		"http://api.twitter.com/1/kiyotou/lists/live/statuses.json?callback=?",
+		null,
+		function(data, status){
+			$.each(data, function(i, item){
+			
+				// write tweets
+				var tweet = '';
+				tweet += '<div class="tweet">';
+				tweet += '<p><span class="username">'+item.user.name+'</span> '+item.text+'</p>';
+				tweet += '<p>'+item.created_at+'</p>';
+				tweet += '</div>';
+				$("#livetweets").append(tweet);
+				
+				// add marker with tweet
+				
+			});
+		}
+	);
 
 	// add Marker
 	$.getJSON("js/livehouse.json", function(data){
@@ -25,21 +48,6 @@ function initialize() {
 		});
 	});
 	
-	// read tweets
-	$.getJSON(
-		"http://api.twitter.com/1/kiyotou/lists/live/statuses.json?callback=?",
-		null,
-		function(data, status){
-			$.each(data, function(i, item){
-				var tweet = '';
-				tweet += '<div class="tweet">';
-				tweet += '<p><span class="username">'+item.user.name+'</span> '+item.text+'</p>';
-				tweet += '<p>'+item.created_at+'</p>';
-				tweet += '</div>';
-				$("#livetweets").append(tweet);
-			});
-		}
-	);
 }
 
 
