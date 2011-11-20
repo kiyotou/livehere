3fbafe470d3a571c622ec5e8978b83c1927cf0ca
kiyotou <kiyotou@gmail.com>
2011-11-20 19:28:52 +0900

    微修正

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -4,7 +4,7 @@ function initialize() {
 	var latlng = new google.maps.LatLng(34.6718, 135.497841);
 	var myOptions = {
 	  zoom: 15,
-	  center: latlng,
+	  // center: latlng,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById("map_minami"), myOptions);
